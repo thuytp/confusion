204084af74684201a027da3ecc0640c6ca02a740
Thuy Tran <tranphuongthuy97@gmail.com>
2022-05-09 22:33:33 +0700

    Lab 02.2 Configure your React Application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import React, { Component } from "react";
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +38,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
