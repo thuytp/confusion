0e925a584a65dd3037c36b3cec36ce89364c4318
Thuy Tran <tranphuongthuy97@gmail.com>
2022-06-12 05:57:46 +0700

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
 import React, { Component } from "react";
-import Menu from "./components/MenuComponent";
+import Main from "./components/MainComponent";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
+        <Main />
       </div>
     );
   }
 }
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 export default App;
